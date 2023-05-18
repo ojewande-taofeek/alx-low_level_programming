78b11b9f87888ea0ff3075f882bf530ea6ed2730
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-18 04:49:08 -0700

    4-puts.c added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main file
+ * Description: a programme to show how to use puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
