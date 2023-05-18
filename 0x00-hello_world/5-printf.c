f3690aac2bf57b7367361929cec1f7e0a29a0997
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-18 04:56:35 -0700

    5-printf.c added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ * Description: a programme to show to how to use printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
