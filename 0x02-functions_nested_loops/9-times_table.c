05fa2c8f9d47a2e487894d28d9adda8bdccfb2f5
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-23 06:24:31 -0700

    9-times_table.c added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,26 +15,26 @@ void times_table(void)
 		for (column = 0; column < 10; column++)
 		{
 			product = row * column;
-			if (product < 10 && column < 9)
+			if (column == 0)
 			{
-				_putchar(product + '0');
+				_putchar(0 + '0');
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
-			else if (product >= 10 && column < 9)
+			else if (product < 10 && (column >= 1))
 			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+				if (column < 9)
+					_putchar(',');
+			}
+			else if (product >= 10)
+			{
+				_putchar(' ');
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (product < 10 && column >= 9)
-				_putchar(product + '0');
-			else if (product > 10 && column >= 9)
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				if (column < 9)
+					_putchar(',');
 			}
 		}
 		_putchar('\n');
