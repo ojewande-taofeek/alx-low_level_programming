e3ba0bd59f14bb666cdd022dea66366e5e16b38e
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-23 06:39:12 -0700

    10-add.c added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - The function adds two integers and returns the result
+ * @a: The first int formal argument
+ * @b: The second int formal argument
+ * Return: The sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
