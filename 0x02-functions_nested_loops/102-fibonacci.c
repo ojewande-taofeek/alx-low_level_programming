aae42c9c26af900926dc84118e427c50e1cda145
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-23 15:51:37 -0700

    102-fibonacci.c added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int first = 1, second = 2, third;
+       	int n;
+
+	printf("1, 2,");
+	for (n = 3; n <= 50; n++)
+	{
+		third = first + second;
+		printf(" %ld", third);
+		if (n < 50)
+			printf(",");
+		first = second;
+		second = third;
+	}
+	printf("\n");
+	return (0);
+}
