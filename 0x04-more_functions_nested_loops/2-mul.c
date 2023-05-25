bc16873e6efdcdb99909f53106b43ca144bd1765
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-25 06:57:20 -0700

    2-mul.c added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - The function multiplies two integers
+ * @a: The first number formal argument of type int
+ * @b: The second number formal argument of type int
+ * Return: the value of multiplied numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
