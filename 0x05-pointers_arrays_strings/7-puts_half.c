0aef0f36ccb673640834321687e3f63301f969da
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-29 14:44:17 -0700

    7-puts_half.c updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,10 @@ void puts_half(char *str)
 
 	for ( ; str[len]; len++)
 		;
-	half = len / 2;
+	if (len % 2 != 0)
+		half = (len - 1) / 2;
+	else
+		half = len / 2;
 	for (; half < len; half++)
 		_putchar(str[half]);
 	_putchar('\n');
