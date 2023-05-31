aac1592e4d6ced90629bafab2935d21919e8bb97
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-05-31 02:27:15 -0700

    2-strncpy.c added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strncpy - The function copies a string of "n" bytes
+ * @dest: The string constant to copy to
+ * @src: The string constant to copy from
+ * @n: The number of bytes to copy from src
+ * Return: The dest string constant
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int idx = 0;
+
+	while (src[idx] && idx < n && n > 0)
+	{
+		dest[idx] = src[idx];
+		idx++;
+	}
+	return (dest);
+}
