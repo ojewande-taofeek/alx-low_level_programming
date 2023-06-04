670e8010b1715efbfde585dedb3e8580ff9e2c46
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-04 08:59:42 -0700

    5-rev_string.c re-added

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * rev_string - The function reverses a string
- * @s: The pointer to a string constant
+ * rev_string - Function that reverses a string
+ * @s: The string constant to reverse
  * Return: Nothing
  */
 
 void rev_string(char *s)
 {
-	int len, idx;
-	char keep;
+	int keep, len, idx, half;
 
 	for (len = 0; s[len]; len++)
 		;
 	len--;
-	for (idx = 0; idx < len; idx++, len--)
+	half = len / 2;
+	for (idx = 0; idx < half; idx++, len--)
 	{
 		keep = s[len];
 		s[len] = s[idx];
