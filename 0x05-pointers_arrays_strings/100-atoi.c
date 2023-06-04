675a083cb7ed52dabefd46ae37499577e38755c8
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-04 11:00:14 -0700

    100-atoi.c added

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - The function converts a string to an integer
+ * @s: The string constant pointer
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int idx = 0, sign = 1, tens = 1, numb, new_numb;
+
+	while (s[idx])
+	{
+		if (s[idx] == '-')
+			sign = -sign;
+		else if (s[idx] >= '0' && s[idx] <= '9')
+		{
+			new_numb = s[idx] - '0';
+			if (tens <= 1)
+				numb = new_numb;
+			else
+				numb = (numb * tens) + new_numb;
+		       tens = 10;
+		}
+		else if ((s[idx - 1] >= '0' && s[idx - 1] <= '9') && s[idx] == ' ')
+			break;
+		idx++;
+	}
+	numb = sign * numb;
+	return (numb);
+}
