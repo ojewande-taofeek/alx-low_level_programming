5b9ea2be571008aec294e43e52718073708af830
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-05 02:21:20 -0700

    1-memcpy.c added

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - Function that copy memory areas
+ * @dest: The steing constant to copy to
+ * @src: The steing constant to copy from
+ * @n: The number of bytes constant to copy from src
+ * Return: The destination steing constant
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int idx = 0;
+
+	while (idx < n)
+	{
+		*dest = src[idx];
+		idx++;
+		dest++;
+	}
+	return (dest);
+}
