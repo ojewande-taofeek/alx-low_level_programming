1fb812142676fd8622d68499c719eb93aadf16e6
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-07 01:15:02 -0700

    0x08-recursion
    main.h and 0-puts_recursion.c added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - The function prints a string, followed by a new line
+ * @s: The pointer to a string constant
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
