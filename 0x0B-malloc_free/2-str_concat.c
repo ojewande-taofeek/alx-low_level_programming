6d7d374ff33df18edb72daf325a41de03b158522
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-13 05:21:36 -0700

    2-str_concat.c - NULL to empty string fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int idx = 0;
 	char *str;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		str = NULL;
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	str = (char *) malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
 	if (str != NULL)
 	{
