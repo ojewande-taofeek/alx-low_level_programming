ed748300817ac609e87c36b342db8e67b4477388
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-15 11:45:18 -0700

    100-realloc.c updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,17 +12,18 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	ptr = malloc(old_size);
-	if (ptr != NULL && new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-		ptr = malloc(new_size);
-	else if (ptr != NULL && (new_size > old_size || new_size < old_size))
-		ptr = malloc(new_size);
-	else if (ptr != NULL && new_size == 0)
+	if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	if (new_size > old_size || new_size < old_size)
+		ptr = malloc(new_size);
+	
 	if (ptr == NULL)
 	{
 		free(ptr);
