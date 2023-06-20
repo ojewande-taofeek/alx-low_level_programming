6b1bd938001ea339163262e59b8ad05e5fec6458
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-20 05:52:59 -0700

    1-init_dog.c added and dog.h updated

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,17 @@
 #include "dog.h"
 
 /**
- * init_dog - The function initialize a variable of type struct dog
- * @d: The variable name of the type struct dog
- * @name: The element "name" of the structure
- * @age: The element "age" of the structure
- * @owner: The element "owner" of the structure
- * Return: Nothing
+ * init_dog - The function initializes a variable of type struct dog
+ * @d: The pointer to the struct dog variable
+ * @name: The pointer to the argument of element name
+ * @age: The argument of element age
+ * @owner: The pointer to the argument of element owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
 		d = malloc(sizeof(struct dog));
-	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
