14444ad240f2e5779043eefa5acea7eeeff33943
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-20 05:04:24 -0700

    dog.h re-added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,12 +5,10 @@
 #include <stdlib.h>
 
 /**
- * struct dog - The structure defines the name, age and owner of a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The dog's owner's name
- * Description: The structure data type cmbines multiple data types
- *              under one data type defined by the user
+ * struct dog - The structure defines the elements of a dog
+ * @name: The element name of type char
+ * @age: The element age of type float
+ * @owner: The element owner of type char
  */
 
 struct dog
@@ -19,15 +17,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - The new name of the struct dog data type
- * Desciption: This is used to illustrate the application of typedef
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
