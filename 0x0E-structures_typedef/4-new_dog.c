e9950b7b0c179f16310023bde0e071067af7b9cd
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-19 21:28:38 -0700

    4-new_dog.c - name and owner copy stored

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,16 +17,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-	new->name = malloc(sizeof(char) * strlen(name));
-	if (new->name == NULL)
+	name = malloc(sizeof(char) * strlen(name));
+	if (name == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 	else
 		new->name = strcpy(new->name, name);
-	new->owner = malloc(sizeof(char) * strlen(owner));
-	if (new->owner == NULL)
+	owner = malloc(sizeof(char) * strlen(owner));
+	if (owner == NULL)
 	{
 		free(new);
 		free(name);
