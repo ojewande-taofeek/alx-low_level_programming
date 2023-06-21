a05fdf8a884c0beb785fb91b71cd79fd6e10acd6
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-21 05:05:46 -0700

    0-print_name.c added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - The function that prints a name
+ * @name: A pointer to a string constant
+ * @f: A funtion pointer that takes name as argument and returns nothing
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
