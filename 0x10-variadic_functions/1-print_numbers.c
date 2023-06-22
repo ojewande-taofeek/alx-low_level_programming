64c29f5f70dda55812a9b8f8167cdcd85a895a4e
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-06-22 05:42:48 -0700

    1-print_numbers.c added

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - The function prints numbers, followed by a new line
+ * @separator: The string constant for the delimiter
+ * @n: The number of arguments to print
+ * Return: exit(0), if separator == NULL || n == 0
+ *         nothing, if successful
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list print_args;
+	unsigned int idx = 0;
+
+	if (separator == NULL || n == 0)
+		exit(0);
+	va_start(print_args, n);
+	while (idx < n)
+	{
+		printf("%d", va_arg(print_args, int));
+		if (idx < (n - 1))
+			printf(", ");
+		idx++;
+	}
+	printf("\n");
+	va_end(print_args);
+}
