a3491819569109aa9a89d3426dc2cbedd6b9f70c
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-03 10:45:14 -0700

    0-print_list.c added

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - The function prints all the elements of a list_t list
+ * Description: If str == NULL, print [0] (nil). Allowed to use printf
+ * @h: Pointer to the head node
+ * Return: the number of elements printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+	const list_t *ptr;
+
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (-1);
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		counter++;
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+	       ptr = ptr->next;
+	}
+	return (counter);
+}
