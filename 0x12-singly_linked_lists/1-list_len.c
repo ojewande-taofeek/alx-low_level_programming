ddf4408099c9419b01356d39a9647a7113e98f38
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-03 10:52:00 -0700

    1-list_len.c added

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - The function that returns the number of elements of a list_t list
+ * @h: Pointer to the head node
+ * Return: the number of elements printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+	const list_t *ptr;
+
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (-1);
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		counter++;
+		ptr = ptr->next;
+	}
+	return (counter);
+}
