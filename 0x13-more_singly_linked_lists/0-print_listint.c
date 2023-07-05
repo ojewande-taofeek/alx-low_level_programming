a99e89c84f68e4630e576ccf5a7eeae6a934e112
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-05 12:24:06 -0700

    0-print_listint.c added

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - The function that prints all the elements of a listint_t
+ * @h: The pointer to the node
+ * Return: Number of nodes(counter), if successful
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+	const listint_t *ptr = malloc(sizeof(listint_t));
+
+	if (ptr == NULL)
+		return (-1);
+	if (h == NULL)
+		return (-1);
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		counter++;
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+	}
+	return (counter);
+}
