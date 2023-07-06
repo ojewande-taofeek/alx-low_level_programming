c1ee8da4acb93ce77e1e7e40584e0c3ee848ed17
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-06 10:25:23 -0700

    2-add_node.c updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - The function returns the length of a string
+ * @s: The pointer to string constant
+ * Return: length of the string (len)
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	for (; s[len]; len++)
+		;
+	return (len);
+}
+
+/**
  * add_node - The function adds a new node at the beginning of a list_t list
  * @head: The pointer to pointer head
  * @str: The string constant pointer
@@ -20,8 +35,8 @@ list_t *add_node(list_t **head, const char *str)
 		free(ptr);
 		return (ptr);
 	}
-	ptr->len = strlen(str);
+	ptr->len = _strlen(str);
 	ptr->next = (*head);
-	(*head = ptr);
+	(*head) = ptr;
 	return (ptr);
 }
