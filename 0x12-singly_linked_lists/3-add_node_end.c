4a31b93103e119d7bcf4304150430793f7873387
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-06 10:31:16 -0700

    3-add_node_end.c updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * _strlen - The function returns the length of a string
+ * @s: The pointer to string constant
+ * Return: length of the string (len)
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	for (; s[len]; len++)
+		;
+	return (len);
+}
+
+
+/**
  * add_node_end - The function adds a new node at the end of a list_t list
  * @head: The pointer to pointer head
  * @str: The string constant pointer
@@ -17,16 +33,19 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	ptr->str = strdup(str);
-	ptr->len = strlen(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->len = _strlen(str);
 	ptr->next = NULL;
-
-	ptr_end = (*head);
-
 	if ((*head) == NULL)
 	{
 		(*head) = ptr;
 		return (ptr);
 	}
+	ptr_end = (*head);
 	while (ptr_end->next != NULL)
 		ptr_end = ptr_end->next;
 	ptr_end->next = ptr;
