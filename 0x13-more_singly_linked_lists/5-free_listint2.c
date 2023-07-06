7520ef228d3be4cfe29c9e038d1867d5a5a5d5c1
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-06 11:55:15 -0700

    5-free_listint2.c - updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp_ptr = (*head);
+	listint_t *tmp_ptr, *ptr = (*head);
 
-	while ((*head) != NULL)
+	while (ptr != NULL)
 	{
-		tmp_ptr = (*head);
-		(*head) = (*head)->next;
-		free(tmp_ptr);
+		tmp_ptr = ptr->next;
+		free(ptr);
+		ptr = tmp_ptr;
 	}
+	(*head) = NULL;
 }
