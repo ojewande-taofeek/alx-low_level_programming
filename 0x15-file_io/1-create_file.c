5b64ccdb83feb41f3232095777c1851cd2815962
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-10 15:59:50 -0700

    1-create_file.c - text_content NULL condition considered

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, r_byte, w_byte;
+	int fd, w_byte;
 
 	fd = open(filename, O_CREAT | O_RDWR  | O_TRUNC, 0600);
 	if ((fd < 0) || (filename == NULL))
 		return (-1);
-	r_byte = read(fd, text_content, strlen(text_content));
-	if (r_byte < 0)
-		return (-1);
-	w_byte = write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+		w_byte = write(fd, text_content, strlen(text_content));
 	if (w_byte < 0)
 		return (-1);
 	close(fd);
