a9057bdedc08bfa75f2c98d9ab43f4d369dd395f
ojewande taofeek <taofeekojewande@nda.edu.ng>
2023-07-10 18:07:41 -0700

    main.h - updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,11 +6,13 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_from_to(const char *file_from, const char *file_to);
+
 
 
 #endif /* MAIN_H */
