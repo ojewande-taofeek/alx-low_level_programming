54cb53acc2d30c47bb0e5672807322e10a53ffee
ojewande-taofeek <taofeekojewande@nda.edu.ng>
2023-08-18 09:00:26 +0000

    2-add_dnodeint.c refactored

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,22 +11,16 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *current = *head;
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
 	new_node->next = *head;
-	current->prev = new_node;
+	new_node->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 	*head = new_node;
 	return (new_node);
 }
