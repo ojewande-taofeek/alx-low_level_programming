5f0efcfec9874d925bb85414b187544cd01369a4
ojewande-taofeek <taofeekojewande@nda.edu.ng>
2023-08-18 10:56:34 +0000

    7-insert_dnodeint.c fixed - all cases considered

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,32 +21,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-	if (*h == NULL || idx == 0)
+	if (*h == NULL)
 	{
-		new_node->next = *h;
-		if (idx == 0 && *h != NULL)
-			current->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
-	while (current->next != NULL)
+	while (current != NULL)
 	{
 		if (i == idx)
 		{
-			new_node->next = current;
 			new_node->prev = current->prev;
-			current->prev->next = new_node;
 			current->prev = new_node;
+			new_node->next = current;
+			if (new_node->prev != NULL)
+				new_node->prev->next = new_node;
+			else
+				*h = new_node;
+			return (new_node);
+		}
+		i++;
+		if (i == idx && current->next == NULL)
+		{
+			current->next = new_node;
+			new_node->prev = current;
 			return (new_node);
 		}
 		current = current->next;
-		i++;
 	}
-	if ((i == idx - 1 || i == idx)  && current->next == NULL)
-	{
-		new_node->prev = current;
-		current->next = new_node;
-		return (new_node);
-		}
 	return (NULL);
 }
