1d0dbc83358a31576eb4bef241545f203ae1c27b
ojewande-taofeek <taofeekojewande@nda.edu.ng>
2023-09-08 09:57:17 +0000

    4-hash_table_get.c added

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - The  function that retrieves a value associated with a key
+ * @ht: The table created
+ * @key: The key
+ * Return: The value mapped to key, if successful
+ *         NULL, if otherwise
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
