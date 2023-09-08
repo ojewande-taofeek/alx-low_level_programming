2796a825c2074607f5d94d12d94bf431011df874
ojewande-taofeek <taofeekojewande@nda.edu.ng>
2023-09-08 16:05:38 +0000

    4-hash_table_get.c updated - if either table or key are NULL considered

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
 	while (current)
