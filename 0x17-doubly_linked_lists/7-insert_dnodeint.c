caceb330e956b5a383f592c3e40d67ca01732dee
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-31 09:47:39 +0100

    updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,8 +30,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			if (idx == 0)
 				return (add_dnodeint(h, n));
-			if (current->next == NULL)
-				return (add_dnodeint_end(h, n));
 			new_node->next = current;
 			new_node->prev = current->prev;
 			current->prev = new_node;
