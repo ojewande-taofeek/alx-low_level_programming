69229857c2f4ad92f3f188db62daa132864e950b
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-18 22:38:01 +0100

    task 3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,35 +1,5 @@
 #include "hash_tables.h"
 
-/**
- * create_node - function that creates a new node
- * @key: The key of the new node
- * @value: The value mapped by the key
- * Return: A pointer to a new node, if successful
- *         NULL, if otherwise
- */
-
-hash_node_t *create_node(char *key, char *value)
-{
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (NULL);
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next = NULL;
-	return (new_node);
-}
 
 /**
  * hash_table_create - function that creates a hash table.
